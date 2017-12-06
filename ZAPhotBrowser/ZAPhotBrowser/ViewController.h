6313ca1ec717d196e66c92771ee7ff6067c536c3
aozou <1527955895@qq.com>
2017-12-06 17:22:51 +0800

    demo

---
+++ ZAPhotBrowser/ZAPhotBrowser/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ZAPhotBrowser
+//
+//  Created by  jiangminjie on 2017/12/6.
+//  Copyright © 2017年 SeaHot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (void)onLog;
+
+@end
+
---
+++ ZAPhotBrowser/ZAPhotBrowser/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  ZAPhotBrowser
+//
+//  Created by  jiangminjie on 2017/12/6.
+//  Copyright © 2017年 SeaHot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)onLog
+{
+    NSLog(@"测试POD");
+}
+
+@end
