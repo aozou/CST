bbb19595bb812ffa2b3095af47a03f787e4eb523
aozou <1527955895@qq.com>
2018-08-13 15:30:29 +0800

    123

---
+++ ZAPhotBrowser/Code/TestShowView.h
@@ -0,0 +1,15 @@
+//
+//  TestShowView.h
+//  ZAPhotBrowser
+//
+//  Created by  jiangminjie on 2018/8/13.
+//  Copyright © 2018年 SeaHot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestShowView : UIView
+
+-(void)onShow;
+
+@end
---
+++ ZAPhotBrowser/Code/TestShowView.m
@@ -0,0 +1,18 @@
+//
+//  TestShowView.m
+//  ZAPhotBrowser
+//
+//  Created by  jiangminjie on 2018/8/13.
+//  Copyright © 2018年 SeaHot. All rights reserved.
+//
+
+#import "TestShowView.h"
+
+@implementation TestShowView
+
+-(void)onShow
+{
+    NSLog(@"***************************************************** \n &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+}
+
+@end
