8d30fca7d7d178dc7864ed2dfe2654e0376282e7
aozou <1527955895@qq.com>
2018-08-13 15:05:18 +0800

    123

--- ZAPhotBrowser/Code/WYNewMainHeaderView.m
+++ ZAPhotBrowser/Code/WYNewMainHeaderView.m
@@ -14,7 +14,6 @@
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #define KScreenWidth  [[UIScreen mainScreen] bounds].size.width            //屏幕宽度
-#define KScreenHeight [[UIScreen mainScreen] bounds].size.height          //屏幕高度
 
 
 @interface WYNewMainHeaderView()<UIScrollViewDelegate> {
